fa4f0145ba656b7268793ac495a544c979db0ded
Joel29t <thos.tornay.joel@gmail.com>
2023-11-30 20:32:41 +0100

    BD changes

--- Base de dades/koloseum_bd.sql
+++ Base de dades/koloseum_bd.sql
@@ -1,48 +1,30 @@
 -- CREATE SCHEMA IF NOT EXISTS `koloseum_bd` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci ;
-
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS koloseum_db;
 USE koloseum_db;
 
--- Table of Users
-drop table Users;
-CREATE TABLE Users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+-- Users table
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
--- Table of Tournaments
-drop table Tournaments;
-CREATE TABLE Tournaments (
-    tournament_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
+-- Tournaments table
+CREATE TABLE tournaments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
     description TEXT,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL
 );
 
--- Table of Inscriptions
-drop table Inscriptions;
-CREATE TABLE Inscriptions (
-    inscription_id INT PRIMARY KEY AUTO_INCREMENT,
+-- Registrations table
+CREATE TABLE registrations (
     user_id INT,
     tournament_id INT,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (tournament_id) REFERENCES Tournaments(tournament_id)
+    PRIMARY KEY (user_id, tournament_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (tournament_id) REFERENCES tournaments(id)
 );
-
--- Table of Results
-drop table Results;
-CREATE TABLE Results (
-    result_id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    tournament_id INT,
-    score INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (tournament_id) REFERENCES Tournaments(tournament_id)
-);
-
-
