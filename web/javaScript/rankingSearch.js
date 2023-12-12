d1e790a5b08b416d5ed448527e14bbcccafb2975
Joel29t <thos.tornay.joel@gmail.com>
2023-12-12 20:08:33 +0100

    search ranking table

--- web/javaScript/rankingSearch.js
+++ web/javaScript/rankingSearch.js
@@ -1,8 +1,36 @@
 $(document).ready(function () {
+
+  let opcionBusqueda = "Player";
+
+  $("#btnBuscarPlayer").click(function () {
+    opcionBusqueda = "Player";
+    $("#btnBuscarPlayer").addClass("btn-primary").removeClass("btn-secondary");
+    $("#btnBuscarOpciones").removeClass("btn-primary").addClass("btn-secondary");
+  });
+
+  $("#btnBuscarOpciones").click(function () {
+    opcionBusqueda = "OtrasOpciones";
+    $("#btnBuscarOpciones").addClass("btn-primary").removeClass("btn-secondary");
+    $("#btnBuscarPlayer").removeClass("btn-primary").addClass("btn-secondary");
+  });
+
   $("#buscar").on("keyup", function () {
-    var value = $(this).val().toLowerCase();
-    $("table tbody tr").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    let value = $(this).val().toLowerCase();
+    $("table tbody tr").each(function () {
+      let mostrarFila = false;
+      if (opcionBusqueda === "Player") {
+        mostrarFila = $(this).find("td:nth-child(2)").text().toLowerCase().indexOf(value) > -1;
+      } else if (opcionBusqueda === "OtrasOpciones") {
+        // Lógica para buscar por otras opciones como PJ, PG, PP, Points
+        for (let i = 3; i <= 6; i++) { 
+          if ($(this).find("td:nth-child(" + i + ")").text().toLowerCase().indexOf(value) > -1) {
+            mostrarFila = true;
+            break;
+          }
+        }
+      }
+
+      $(this).toggle(mostrarFila);
     });
   });
 });
